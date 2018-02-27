d3e4c8a9202cfef71ac0e5b65a00bc8557966754
Keith Amundson <amundskm@gmail.com>
2018-02-27 09:45:21 -0600

    Created basic layout of calculator in html and css. wrote plan enter button in  script

---
+++ script/script-1.js
@@ -0,0 +1,13 @@
+// psuedo code //
+/* Check if there is any numbers in front of '(' without
+multiplication symbol, or behind ')', add muliplication. 
+Example: (4+2)2 -> (4+2)x2 */
+
+/* While there are any '(' in the expression, grab the
+string between the first set of (). Create function so it
+is recursive */
+
+/*while there are any operators in the string, call functions
+in the correct operational order to solve the expression
+multiplication, division, addition, subtraction. return a
+single number as the expression. */
